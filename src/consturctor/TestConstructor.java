1478319ae87f44596729eb4153f80a5d99740542
Mayur900 <128124045+Mayur900@users.noreply.github.com>
2023-03-18 15:50:38 +0530

    xs

--- src/consturctor/TestConstructor.java
+++ src/consturctor/TestConstructor.java
@@ -3,7 +3,7 @@ package consturctor;
 public class TestConstructor extends Sub{
 	public static void main(String[] args) {
 		//Sub s = new Sub();
-		Add a = new Sub(1,2);
+		Add a = new Sub(20,14);
 		Divide s= new  Multiplay(6,7);
 		
 	}
