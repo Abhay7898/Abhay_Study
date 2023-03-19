d7377f432389826e5216ea1f36b3f68f72dba52c
Abhay7898 <128139505+Abhay7898@users.noreply.github.com>
2023-03-19 16:53:03 +0530

    new

--- src/interFace/Colours.java
+++ src/interFace/Colours.java
@@ -1,28 +1,7 @@
 package interFace;
 
 public class Colours implements A {
-	public void red() {
-		System.out.println("Red");
-	}
-		public void yellow() {
-			System.out.println("Yellow");
-		}
-		public void blue() {
-			System.out.println("Blue");
-		}
-		public void black() {
-			System.out.println("Black");
-		}
-		public void green() {
-			System.out.println("Green");
-		}
-		public void orange() {
-			System.out.println("Orange");
-		}
-		public void pink() {
-			System.out.println("Pink");
-		}
-
+	
 	
 	
 	
