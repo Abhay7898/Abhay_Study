a7f1d15c1b892f5380c2d4159126a9079fd5b01e
Abhay7898 <128139505+Abhay7898@users.noreply.github.com>
2023-06-20 17:40:44 +0530

    hi

--- src/multithread_20_June/RunableWithFuncnalInterface.java
+++ src/multithread_20_June/RunableWithFuncnalInterface.java
@@ -1,7 +1,6 @@
 package multithread_20_June;
 
 public class RunableWithFuncnalInterface {
-
 	static void print() {
 		System.out.println("Hello");
 		
@@ -10,29 +9,15 @@ public class RunableWithFuncnalInterface {
 				System.out.println(i);
 			}
 		}
-		
-		System.out.println("Bye");
-
 	}
-
 	public static void main(String[] args) {
-
 		Runnable r = () -> {
 			print();
 		};
-
 		Thread t = new Thread(r);
 		t.start();
-		synchronized (t) {
-			for (int i = 0; i < 10; i++) {
-				System.out.println(i);
-			}
-		}
-		
 
 		Thread t1 = new Thread(r);
 		t1.start();
-
 	}
-
 }
